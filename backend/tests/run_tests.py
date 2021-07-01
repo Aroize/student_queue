c7f5e299bc8a398aabd64b8da5549b29abe8ed41
ARQtty <ShamovIA@yandex.ru>
2021-07-01 16:14:46 +0300

    Make exit code 1 if any test failed

--- backend/tests/run_tests.py
+++ backend/tests/run_tests.py
@@ -9,5 +9,11 @@ import security_tests
 if __name__ == '__main__':
     suites = [unittest.TestLoader().loadTestsFromModule(security_tests), ]
 
+    failed = False
     for suite in suites:
-        unittest.TextTestRunner(verbosity=2).run(suite)
+        result = unittest.TextTestRunner(verbosity=2).run(suite)
+        if not result.wasSuccessful():
+            failed = True
+
+    if failed:
+        exit(1)
